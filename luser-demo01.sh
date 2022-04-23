207988a8b65fa61351d7d66fbccf6dbf668a7068
drcrazykid <43323243+drcrazykid@users.noreply.github.com>
2022-04-23 01:12:09 -0400

    started
    getting through the stuff I already know z_z

---
+++ luser-demo01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script displays various information to the screen.
+
+# Display 'Hello'
+echo 'Hello'
+
+# Assign a value to a variable
+WORD='script'
+
+# Display the value of the variable
+
+echo "$WORD"
