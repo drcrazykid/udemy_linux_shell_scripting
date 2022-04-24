fd01bd5bca447299f050de9b0756a30936c12a23
drcrazykid <43323243+drcrazykid@users.noreply.github.com>
2022-04-24 02:12:04 -0400

    completed more lessons

--- luser-demo01.sh
+++ luser-demo01.sh
@@ -9,5 +9,30 @@ echo 'Hello'
 WORD='script'
 
 # Display the value of the variable
-
 echo "$WORD"
+
+# Display an example with single quotes
+echo '$WORD'
+
+# Combine the variable with hard coded text.
+echo "This is a shell $WORD"
+
+# Display the contents of the variable using an alternative syntax.
+echo "This is a shell {$WORD}"
+
+# Append text to the variable
+echo "${WORD}ing is fun"
+
+# What not to do
+echo "$WORDing is fun"
+
+# Create a new variable
+ENDING='ed'
+
+# Combine the two variables
+echo "This is ${WORD}${ENDING}"
+
+# Change the value stored int hte ENDING variable. (Reassignment)
+ENDING='ing'
+echo "${WORD}${ENDING} is fun"
+
